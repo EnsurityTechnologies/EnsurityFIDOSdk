7de81c4c2fbf43ed751f472f0332b6d6c2ad7d40
vishnuvardhan.u <vishnuvardhan.u@ensurity.com>
2025-07-25 16:03:16 +0530

    initial commit

---
+++ Clients/FidoApiClient.cs
@@ -0,0 +1,47 @@
+﻿using FidoClientSdk.Extensions;
+using FidoClientSdk.Interfaces;
+using FidoClientSdk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FidoClientSdk.Clients
+{
+    public class FidoApiClient : IFidoApiClient
+    {
+        private readonly HttpClient _httpClient;
+        public FidoApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<BaseResponse> ServerCredentialsRequestAsync(ServerCredentialsDto request)
+        {
+            return await FidoInternalUtils.CredentialRequest(request);
+        }
+
+        public async Task<MakeCredentialResponse> MakeCredentialRequestAsync(MakeCredentialRequest request)
+        {
+            return await FidoInternalUtils.MakeCredential(request, _httpClient);
+        }
+
+        public async Task<BaseResponse> MakeCredentialResponseAsync(MakeCredentialFinishRequest responseData)
+        {
+            return await FidoInternalUtils.MakeCredentialResp(responseData, _httpClient);
+        }
+
+        public async Task<GetAssertionResponse> GetAssertionRequestAsync(GetAssertionRequest requestData)
+        {
+            return await FidoInternalUtils.GetAssertion(requestData, _httpClient);
+        }
+
+        public async Task<BaseResponse> GetAssertionResponseAsync(GetAssertionFinishRequest responseData)
+        {
+            return await FidoInternalUtils.GetAssertionResp(responseData, _httpClient);
+        }
+    }
+}
---
+++ Interfaces/IFidoApiClient.cs
@@ -0,0 +1,18 @@
+﻿using FidoClientSdk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FidoClientSdk.Interfaces
+{
+    public interface IFidoApiClient
+    {
+        Task<BaseResponse> ServerCredentialsRequestAsync(ServerCredentialsDto request);
+        Task<MakeCredentialResponse> MakeCredentialRequestAsync(MakeCredentialRequest request);
+        Task<BaseResponse> MakeCredentialResponseAsync(MakeCredentialFinishRequest responseData);
+        Task<GetAssertionResponse> GetAssertionRequestAsync(GetAssertionRequest requestData);
+        Task<BaseResponse> GetAssertionResponseAsync(GetAssertionFinishRequest responseData);
+    }
+}
